a6bbd70e2dbeeeb81e5421557a123c71f52b9dda
makoto abe <abe00makoto@gmail.com>
2017-08-12 22:35:17 +0900

    update redux-observable v0.15

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,25 +1,7 @@
-import { Observable } from 'rxjs';
 import { Action, ActionCreator } from 'typescript-fsa';
-import { MiddlewareAPI, Middleware } from 'redux';
 import 'rxjs/add/operator/filter';
 declare module 'redux-observable' {
     interface ActionsObservable<T> {
         ofAction<T, P>(action: ActionCreator<P>): ActionsObservable<Action<P>>;
     }
-    interface EpicWithD<T, S, D> {
-        (action$: ActionsObservable<T>, store: MiddlewareAPI<S>, depends: D): Observable<T>;
-    }
-    interface EpicMiddlewareWithD<T, S, D> extends Middleware {
-        replaceEpic(nextEpic: EpicWithD<T, S, D>): void;
-    }
-    interface AdapterOption {
-        input: (input$: Observable<any>) => any;
-        output: (output$: any) => Observable<any>;
-    }
-    interface OptionsWithD<D> {
-        adapter?: AdapterOption;
-        dependencies?: D;
-    }
-    function createEpicMiddleware<T, S, D>(rootEpic: EpicWithD<T, S, D>, options?: OptionsWithD<D>): EpicMiddlewareWithD<T, S, D>;
-    function combineEpics<T, S, D>(...epics: EpicWithD<T, S, D>[]): EpicWithD<T, S, D>;
 }
--- lib/index.ts
+++ lib/index.ts
@@ -9,30 +9,6 @@ declare module 'redux-observable' {
     interface ActionsObservable<T> {
         ofAction<T, P>(action: ActionCreator<P>): ActionsObservable<Action<P>>;
     }
-
-    // Epic With Dependencies
-    interface EpicWithD<T, S, D> {
-        (action$: ActionsObservable<T>, store: MiddlewareAPI<S>, depends: D): Observable<T>;
-    }
-
-    // EpicMiddleware With Dependencies
-    interface EpicMiddlewareWithD<T, S, D> extends Middleware {
-        replaceEpic(nextEpic: EpicWithD<T, S, D>): void;
-    }
-    interface AdapterOption {
-        input: (input$: Observable<any>) => any;
-        output: (output$: any) => Observable<any>;
-      }
-      
-    interface OptionsWithD<D> {
-        adapter?: AdapterOption;
-        dependencies?: D;
-    }
-    function createEpicMiddleware<T, S, D>(
-        rootEpic: EpicWithD<T, S, D>,
-        options?: OptionsWithD<D>): EpicMiddlewareWithD<T, S, D>;
-
-    export function combineEpics<T, S, D>(...epics: EpicWithD<T, S, D>[]): EpicWithD<T, S, D>;
 }
 
 ActionsObservable.prototype.ofAction =
