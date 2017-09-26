abd1b5da53ef7a58f7fb4ac220ab3286eb34a200
makoto abe <abe00makoto@gmail.com>
2017-09-26 19:22:52 +0900

    delete dist/

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Action, ActionCreator } from 'typescript-fsa';
-import 'rxjs/add/operator/filter';
-declare module 'redux-observable' {
-    interface ActionsObservable<T> {
-        ofAction<T, P>(action: ActionCreator<P>): ActionsObservable<Action<P>>;
-    }
-}
--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const redux_observable_1 = require("redux-observable");
-const typescript_fsa_1 = require("typescript-fsa");
-require("rxjs/add/operator/filter");
-redux_observable_1.ActionsObservable.prototype.ofAction =
-    function (actionCreater) {
-        return this.filter(action => (typescript_fsa_1.isType(action, actionCreater)));
-    };
